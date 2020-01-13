ba4c23b4431392225b38642ff33d541e3c4187c7
mmyadav <mintu08.yadav@gmail.com>
2020-01-13 12:05:48 +0530

    v1

---
+++ src/components/counter.jsx
@@ -0,0 +1,39 @@
+/** @format */
+
+import React, { Component } from "react";
+class Counter extends Component {
+  state = {
+    value: this.props.counter.value
+  };
+  handleevent = () => {
+    this.setState({ value: this.state.value + 1 });
+  };
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button onClick={this.handleevent} className='btn btn-secondary btn-sm'>
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className='btn btn-danger btn-sm m-2'
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    // const {value}=this.state;
+    return this.state.value === 0 ? "Zero" : this.state.value;
+  }
+}
+
+export default Counter;
